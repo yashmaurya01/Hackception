0809d064f4e34f82c39cf020e745839c0c51b8e8
Yash Maurya <maurya.yash152@gmail.com>
2024-11-09 18:41:25 -0500

    basic setup frontend react

---
+++ frontend/src/index copy.js
@@ -0,0 +1,8 @@
+// frontend/src/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './styles.css'; // You'll need to create a styles.css file
+
+ReactDOM.render(<App />, document.getElementById('root'));
